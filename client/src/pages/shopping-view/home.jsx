91d707908fc165b6c5f0e35343e3976cd76e71df
Atharvaw07 <102822056+Atharvaw07@users.noreply.github.com>
2024-11-08 23:32:32 +0530

    Update home.jsx

--- client/src/pages/shopping-view/home.jsx
+++ client/src/pages/shopping-view/home.jsx
@@ -122,7 +122,7 @@ function ShoppingHome() {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="relative w-full h-[600px] overflow-hidden">
+      <div className="relative w-full sm:h-[200px] md:h-[400px] lg:h-[600px] overflow-hidden">
         {featureImageList && featureImageList.length > 0
           ? featureImageList.map((slide, index) => (
               <img
